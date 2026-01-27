68afc7ea134afbd5d3686f95a035f16063ba1ef5
DARINHIL.THA <DARINHIL.THA@pnc.local>
2026-01-27 07:31:42 +0700

    contaact-page

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -6,9 +6,11 @@
     <title>Laptop Store</title>
     <link rel="stylesheet" href="/app/views/css/home.css">
     <link rel="stylesheet" href="/app/views/css/navbar.css">
+    <link rel="stylesheet" href="/app/views/css/contact.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
